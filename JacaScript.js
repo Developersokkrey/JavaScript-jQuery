f511f0598fd7f059d399c6c54ee86edf11562cbf
Developersokkrey <72006616+Developersokkrey@users.noreply.github.com>
2024-02-06 16:05:28 +0700

    Update JacaScript.js

--- JacaScript.js
+++ JacaScript.js
@@ -1,5 +1,29 @@
-// JavaScript Ternary Operator
-let speed = 150;
-let message = speed >= 120 ? 'Too Fast' : speed >= 80 ? 'Fast' : 'OK';
+//--------------------------------------- JavaScript Ternary Operator
+  let speed = 150;
+  let message = speed >= 120 ? 'Too Fast' : speed >= 80 ? 'Fast' : 'OK';
+  console.log(message);
 
-console.log(message);
+//-------------------------------------- JavaScript Object Spread
+  //-- 1
+    let colors = ['red', 'green', 'blue','df','sdf','weg'];
+    let rgb = [...colors];
+    let afrom = Array.from(colors);
+    console.log(afrom);
+    console.log([...colors]);
+      // * Output
+      // [ 'red', 'green', 'blue', 'df', 'sdf', 'weg' ]
+  //-- 2
+    const circle = {
+        radius: 10
+    };
+    const coloredCircle = {
+        ...circle,
+        color: 'black'
+    };
+    console.log(coloredCircle);
+      // * Output
+      // {
+      //   radius: 10,
+      //   color: 'black'
+      // }
+
