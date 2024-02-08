3c5e9d18916769112f381f1e46aefc32f4105c65
Developersokkrey <72006616+Developersokkrey@users.noreply.github.com>
2024-02-08 13:57:19 +0700

    Update JacaScript.js

--- JacaScript.js
+++ JacaScript.js
@@ -223,6 +223,28 @@
         let show = () => console.log('Anonymous function');
         ----
         let add = (a, b) => a + b;   
+//-------------------------------------- JavaScript Callbacks
+
+        function isOdd(number) {
+          return number % 2 != 0;
+        }
+        function isEven(number) {
+          return number % 2 == 0;
+        }
+        
+        function filter(numbers, fn) {
+          let results = [];
+          for (const number of numbers) {
+            if (fn(number)) {
+              results.push(number);
+            }
+          }
+          return results;
+        }
+        let numbers = [1, 2, 4, 7, 3, 5, 6];
+        
+        console.log(filter(numbers, isOdd));
+        console.log(filter(numbers, isEven));
 
 //-------------------------------------- Basic operations on arrays
 
