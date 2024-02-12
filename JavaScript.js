c651659e799aa48dfef203a29e9bb417bbf1d07b
Developersokkrey <72006616+Developersokkrey@users.noreply.github.com>
2024-02-12 16:48:07 +0700

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -555,3 +555,38 @@
       console.log(result);
       // * Output
                    // true
+
+#Sort
+#Array
+//-------------------------------------- JavaScript Array Sorting an array
+//-- #1 JavaScript Sorting an array
+      let employees = [
+          {name: 'John', salary: 90000, hireDate: "July 1, 2010"},
+          {name: 'David', salary: 75000, hireDate: "August 15, 2009"},
+          {name: 'Ana', salary: 80000, hireDate: "December 12, 2011"}
+      ];
+      // sort by salary
+      employees.sort(function (x, y) {
+          return x.salary - y.salary;
+      });
+      console.table(employees);
+      // * Output
+                   // Sort by salary ads
+//-- #2 JavaScript Array some
+       employees.sort(function (x, y) {
+            let a = x.name.toUpperCase(),
+                b = y.name.toUpperCase();
+            return a == b ? 0 : a > b ? 1 : -1;
+        });
+        console.table(employees);
+      // * Output
+                   // Sort by name ads
+//-- #2 JavaScript Array some
+       employees.sort(function (x, y) {
+            let a = new Date(x.hireDate),
+                b = new Date(y.hireDate);
+            return a - b;
+        });
+        console.table(employees);
+      // * Output
+                   // Sort by name ads
